3d33288aedebcd247e12a77e37e5265fe5594162
Nicholas Jones <njonesgrad@gmail.com>
2025-04-30 22:07:58 +0200

    Manifest.json + Post Interface

--- frontend/src/interfaces/IPostInfo.ts
+++ frontend/src/interfaces/IPostInfo.ts
@@ -1,12 +1,9 @@
 export interface IPostInfo {
-    id: string;
-    author: string;
-    handle: string;
-    content: string;
-    timestamp: string;
-    likes: string;
-    retweets: string;
-    element: HTMLElement;
-    filtered?: boolean; 
-  }
-  
+  id: string;
+  author: string;
+  content: string;
+  timestamp: string;
+  likes: string;
+  retweets: string;
+  element: HTMLElement;
+}
