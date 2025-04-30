fb390ee403c40ef8b5fb7df23fcff0403a8f29aa
Nicholas Jones <njonesgrad@gmail.com>
2025-04-30 22:06:58 +0200

    Log Post Info for Testing

--- frontend/src/utils/logPostInfo.ts
+++ frontend/src/utils/logPostInfo.ts
@@ -3,10 +3,10 @@ import { IPostInfo } from '../interfaces/IPostInfo';
 /**
  * Log post information to console
  */
-export const logPostInfo = (postInfo: IPostInfo) => {
-  console.log('======= X-FILTER: FIRST TWEET =======');
+export const logPostInfo = (postInfo: IPostInfo, header: string) => {
+  console.log(`========== ${header}===========`);
   console.log(`ID: ${postInfo.id}`);
-  console.log(`Author: ${postInfo.author} (${postInfo.handle})`);
+  console.log(`Author: ${postInfo.author}`);
   console.log(`Posted: ${postInfo.timestamp}`);
   console.log(`Content: ${postInfo.content}`);
   console.log(`Likes: ${postInfo.likes}, Retweets: ${postInfo.retweets}`);
